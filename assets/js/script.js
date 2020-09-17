e0ef038431a36693d3525b24b75aae3d0087ad46
EMIRT32 <emirt.1993@gmail.com>
2020-09-17 09:10:20 -0500

    clase jueves 17 de septiembre

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,42 @@
-const dark = document.getElementById('dark')
-const light = document.getElementById('light')
+const dark = document.getElementById('dark');
+const light = document.getElementById('light');
+const pink = document.getElementById('pink');
+const ciand = document.getElementById('cian');
+
+dark.addEventListener('click', tema=>{
+    container.className = "conatiner dark"
+   // document.getElementById('container').classList.add('dark');
+    localStorage.setItem('tema', 'dark');
+});
+light.addEventListener('click', tema=>{
+    container.className = "conatiner light"
+   // document.getElementById('container').classList.remove('dark');
+    localStorage.setItem('tema', 'light');
+});
+pink.addEventListener('click', ()=>{
+    container.className = "container pink"
+    localStorage.setItem('tema', 'pink')
+})
+
+
+const temaEnStorage = ()=>{
+    const temaGuardado = localStorage.getItem('tema') ;
+    if (temaGuardado == 'dark'){
+        
+        container.classList.add('dark');
+    } else if (temaGuardado == 'light'){
+        container.className = 'container';
+        //container.classList.remove('dark');
+    } else if (temaGuardado == 'pink'){
+        container.classList.add('pink');
+    }
+    console.log(temaGuardado);
+}
+temaEnStorage();
+
+
+
+/*
 dark.addEventListener('click', tema=>{
     document.getElementById('container').classList.add('dark')
     localStorage.removeItem('ligth');
@@ -26,3 +63,4 @@ function tema(){
         document.getElementById('container').classList.add('light');
     } 
 }
+*/
